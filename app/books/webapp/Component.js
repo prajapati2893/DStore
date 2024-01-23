e35d31f61f567f90d3bb654f7b400c2ad2440f42
lucifer <indradhanush60@gmail.com>
2024-01-23 11:00:22 +0530

    title fix in object page

---
+++ app/books/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/fe/core/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("msg.dstore.books.books.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
